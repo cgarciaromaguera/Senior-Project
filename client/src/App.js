46c9c00eb72ac45217abd05a768ac3e075bf6332
cgarciaromaguera <cgarciaromaguera@ufl.edu>
2024-02-28 17:04:45 -0500

    SignUp Page

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,19 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 //import logo from './logo.png';
 
 //pages & components 
-import Login from './pages/Login'
+import Login from './pages/Login';
 import Navbar from './components /Navbar';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
         <div className="pages">
           <Routes>
             <Route
               path="/"
-              element={<Login />}
+              element={<SignUp />}
             />
           </Routes>
         </div>
