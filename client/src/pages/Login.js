d940940b548329bcb2e7165fa330fc37acb3e5e5
cgarciaromaguera <cgarciaromaguera@ufl.edu>
2024-02-28 15:12:42 -0500

    Login page & Logo

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,18 @@
+import './Login.css'; 
+import logo from './logo.png';
+
 const Login = () => {
     return (
-    <div className="login">
-        <h2>Login</h2>
-    </div>
+        <div className="login">
+            <img src={logo} alt="Logo" className="logo" /> {/* Include the logo */}
+            <h2>Login</h2>
+            <div className="input-container">
+                <input type="text" placeholder="Username" />
+            </div>
+            <div className="input-container">
+                <input type="password" placeholder="Password" />
+            </div>
+        </div>
     )
-}
-
-export default Login
+  }  
+  export default Login;
