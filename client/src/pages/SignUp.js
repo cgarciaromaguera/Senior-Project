254fdcbc232e87f352ca4fe2e3f58a9521ce9627
cgarciaromaguera <cgarciaromaguera@ufl.edu>
2024-02-28 15:59:40 -0500

    Sign Up Pages

---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,10 @@
+import './SignUp.css'; 
+
+const SignUp = () => {
+    return (
+        <div className="signup">
+            <h2>Sign Up</h2>
+        </div>
+    )
+  }  
+  export default SignUp;
