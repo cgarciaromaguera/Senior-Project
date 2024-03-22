6a70d54ec83ccd0201d85ed7859159dc3c50228c
Victoria Moya <victoriam0388@gmail.com>
2024-03-22 13:14:56 -0400

    nejkcn

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,39 +1,25 @@
 // ... other imports
-import { useState } from 'react'
-import { useLogin } from '../hooks/useLogin';
 import './Login.css'; 
 import logo from './logo.png';
 
 const Login = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const {login, error, isLoading} = useLogin()
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    
-    console.log("before login hook")
-    await login(username, password)
-  }
-
   return (
-    <form>
+    <div className='wrapper'>
       <div className="login">
-        <img src={logo} alt="Logo" className="logo-login" />
-        <div className="input-container-login">
-          <input type="text" placeholder="Username" onChange={(e) => {setUsername(e.target.value)}}/>
+        <img src={logo} alt="Logo" className="logo" />
+        <div className="input-container">
+          <input type="text" placeholder="Username" />
         </div>
-        <div className="input-container-login">
-          <input type="password" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}}/>
+        <div className="input-container">
+          <input type="password" placeholder="Password" />
         </div>
-        <button className="login-button" onSubmit={handleSubmit}>Login</button> {/* Login button */}
+        <button className="login-button">Login</button> {/* Login button */}
         <div className="login-footer">
           <a href="/forgot-password" className="forgot-password">Forgot Password?</a>
-          <a href="/signup" className="sign-up">Don't have an account? Sign Up</a>
+          <a href="/sign-up" className="sign-up">Don't have an account? Sign Up</a>
         </div>
-        
       </div>
-    </form>
+    </div>
   )
 }  
 
